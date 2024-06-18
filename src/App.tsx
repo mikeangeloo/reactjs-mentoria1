f57c1b7c36a9846453771a2ee89a742cf33e6625
Miguel Angel Ramirez Lopez <C-MiguelAngel.Ramirez@charter.com>
2024-06-18 09:22:34 -0500

    Adding use effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,91 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./api-users.css";
+import "./loader.css";
 
-type User = {
+type ApiUser = {
+  createdAt: string;
   name: string;
-  lastname: string;
-  age: number;
+  avatar: string;
+  id: string;
 };
 
+type Loading = {
+  isLoading: boolean;
+};
+
+const apiURL = "https://6663665862966e20ef0c7f22.mockapi.io/api/v1";
+
+const loader = (
+  <div className="lds-roller">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
 function App() {
-  const [count, setCount] = useState(0);
-  const [user, setUser] = useState<User>({
-    name: "Miguel",
-    lastname: "Ramírez",
-    age: 25,
+  const [users, setApiUsers] = useState<ApiUser[]>();
+  const [loading, setLoading] = useState<Loading>({
+    isLoading: false,
   });
 
-  const handleChangeUser = ({ name, lastname, age }: User) => {
-    setUser({
-      name,
-      lastname,
-      age,
-    });
-  };
-
-  console.log("user data --->", JSON.stringify(user));
+  useEffect(() => {
+    setLoading({ isLoading: true });
+    fetch(`${apiURL}/products`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTimeout(() => {
+          setApiUsers(data);
+          setLoading({ isLoading: false });
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("Error fetching data -->", error);
+        setLoading({ isLoading: false });
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="table-container">
+        {loading.isLoading ? (
+          loader
+        ) : (
+          <table>
+            <caption>Api Users</caption>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Photo</th>
+                <th>Created At</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users?.map((user) => (
+                <tr id={user.id}>
+                  <td>{user.id}</td>
+                  <td>{user.name}</td>
+                  <td>
+                    <img
+                      className="avatar"
+                      id={user.id}
+                      src={user.avatar}
+                      alt="User Avatar"
+                    />
+                  </td>
+                  <td>{new Date(user.createdAt).toDateString()}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
-      <h1>Starting Project</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-
-      <div className="user">
-        <p>User name: {user?.name}</p>
-        <p>User lastname: {user?.lastname}</p>
-        <p>User age: {user?.age}</p>
-
-        <button
-          onClick={() =>
-            handleChangeUser({
-              name: "Francisco",
-              lastname: "Gonzalez",
-              age: 22,
-            })
-          }
-        >
-          Change Username
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
